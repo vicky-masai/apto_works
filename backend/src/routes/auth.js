7c486f036176eb44688c067275eff4b5a1778a3b
Suraj Pandey <surajpandey7493@gmail.com>
2025-04-08 16:12:58 +0530

    Get UserProfile api created

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { auth } = require('../middleware/auth');
 
 // Registration
 router.post('/register', authController.register);
@@ -15,5 +16,8 @@ router.post('/resend-otp', authController.resendOTP);
 // Password Reset
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
+
+// User Profile
+router.get('/profile',auth ,authController.getUserProfile);
 
 module.exports = router; 
