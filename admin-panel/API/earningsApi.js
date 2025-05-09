2ea2d5e9116ca0c8af40b56394942d2c057cee29
Suraj Pandey <surajpandey7493@gmail.com>
2025-05-09 12:37:38 +0530

    api access using env

--- admin-panel/API/earningsApi.js
+++ admin-panel/API/earningsApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const fetchEarningsData = async () => {
   try {
-    const response = await axios.get("http://localhost:4000/api/admin/earnings-v2");
+    const response = await axios.get(`${BASE_URL}/admin/earnings-v2`);
     return response.data;
   } catch (error) {
     console.error("Error fetching earnings data:", error);
